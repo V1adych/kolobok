7c839637ce9a2559cb534ab1a4cfa10bb079ca25
Nikita <nikitazag3@gmail.com>
2025-06-29 10:02:08 +0000

    Update scripts & notebooks

--- ml/scripts/train_depth_scenarios.py
+++ ml/scripts/train_depth_scenarios.py
@@ -4,16 +4,33 @@ from itertools import product
 
 def main():
     models = ["swin_v2_s", "effnet_b7", "densenet201", "googlenet"]
-    with_synthetic = [True, False]
-    with_aug = [True, False]
-    with_clahe = [True, False]
+    with_scheduler = [True]
+    gradient_clip = [None]
+    with_synthetic = [True]
+    with_aug = [False]
+    with_clahe = [False]
+    num_epochs = [50]
 
-    for model, with_synthetic, with_aug, with_clahe in product(
-        models, with_synthetic, with_aug, with_clahe
+    for (
+        model,
+        with_synthetic,
+        with_aug,
+        with_clahe,
+        with_scheduler,
+        gradient_clip,
+        num_epochs,
+    ) in product(
+        models,
+        with_synthetic,
+        with_aug,
+        with_clahe,
+        with_scheduler,
+        gradient_clip,
+        num_epochs,
     ):
         cmd = [
             "python",
-            "scripts/train_depth_regression.py",
+            "scripts/train_depth_classification.py",
             "--model_name",
             model,
         ]
@@ -24,7 +41,11 @@ def main():
             cmd.append("--with_aug")
         if with_clahe:
             cmd.append("--with_clahe")
-
+        if with_scheduler:
+            cmd.append("--use_scheduler")
+        if gradient_clip is not None:
+            cmd.append(f"--gradient_clip={gradient_clip}")
+        cmd.append(f"--num_epochs={num_epochs}")
         print(" ".join(cmd))
 
         subprocess.run(cmd)
