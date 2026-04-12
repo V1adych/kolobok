132c774b39c416a58bc30f8a31e3c5b04311a663
Nikita <nikitazag3@gmail.com>
2026-04-12 14:17:03 +0300

    add setup script

---
+++ ml/scripts/setup_mmdet.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -euo pipefail
+
+pip install mmcv==2.2.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.4/index.html
+pip install mmengine tensorboard tensorboardX
+pip install -r submodules/mmdetection/requirements/runtime.txt
+pip install -e submodules/mmdetection
