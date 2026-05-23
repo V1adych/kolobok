1954e7df1a2c2ef8e3e768d1ae74422d1a323e97
Nikita <184722040+V1adych@users.noreply.github.com>
2026-05-23 16:46:05 +0300

    remove extra line

--- ml/scripts/setup_mmdet.sh
+++ ml/scripts/setup_mmdet.sh
@@ -3,7 +3,6 @@
 set -euo pipefail
 
 pip install mmcv==2.2.0 -f https://download.openmmlab.com/mmcv/dist/cu121/torch2.4/index.html
-pip install mmcv==2.1.0 -f https://download.openmmlab.com/mmcv/dist/cpu/torch2.1/index.html
 pip install mmengine tensorboard tensorboardX
 pip install -r submodules/mmdetection/requirements/runtime.txt
 pip install albumentations roboflow
